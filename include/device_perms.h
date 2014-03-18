447dc3234ee0c717d5dda4813d4c8fdb69c7da2c
dhacker29 <dhackerdvm@gmail.com>
2014-03-18 04:42:55 -0400

    Fix permissions for ril-daemon1 for dual sim devices
    Change-Id: Ib8e7e04ddc6dee61607f4c43528f59b7f4037c93
    
    Conflicts:
    	include/device_perms.h
    	rootdir/etc/init.class_main.sh

--- include/device_perms.h
+++ include/device_perms.h
@@ -34,8 +34,7 @@
 
 #define CONTROL_PERMS_APPEND \
     { "mmi-ril-daemon",AID_RADIO, AID_RADIO }, \
-    { "ril-daemon1", AID_RADIO, 0 }, \
-    { "qmuxd", AID_RADIO, 0 }, \
-    { "netmgrd", AID_RADIO, 0 },
+    { "ril-daemon1",AID_RADIO, AID_RADIO }, \
+    { "ril-daemon2",AID_RADIO, AID_RADIO },
 
 #endif /* DEVICE_PERMS_H */
